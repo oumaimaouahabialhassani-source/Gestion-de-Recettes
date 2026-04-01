1c3c07ab90cf7561b667fcc780039d9361cf9f94
oumaimaouahabialhassani-source <oumaimaouahabialhassani@gmail.com>
2026-04-01 12:26:30 +0100

    Add files via upload

---
+++ PDO/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'dbblog';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
